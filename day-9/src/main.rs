fc5453149138191591814d834ae72297524bbed6
joeggg <47352958+joeggg@users.noreply.github.com>
2025-12-23 00:26:59 +0000

    day 9 part 2 brute force

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -9,27 +9,39 @@ fn main() {
 
 fn get_largest_rectangle_area(tiles: Vec<(u64, u64)>) -> u64 {
     let mut borders: HashSet<(u64, u64)> = HashSet::new();
+    // Record all the tiles that form the border of the green area
     for (i, (x_1, y_1)) in tiles.iter().enumerate() {
+        // Include the red tiles
+        borders.insert((*x_1, *y_1));
         for (x_2, y_2) in tiles.iter().skip(i + 1) {
+            // If 2 red tiles are on the same axis, all tiles between them will be green and part
+            // of the border
             if x_1 == x_2 {
                 let upper = if y_1 > y_2 { y_1 } else { y_2 };
                 let lower = if y_1 < y_2 { y_1 } else { y_2 };
-                for i in (*lower + 1)..*upper {
-                    borders.insert((*x_1, i));
+                for n in (*lower + 1)..*upper {
+                    borders.insert((*x_1, n));
                 }
             } else if y_1 == y_2 {
                 let upper = if x_1 > x_2 { x_1 } else { x_2 };
                 let lower = if x_1 < x_2 { x_1 } else { x_2 };
-                for i in (*lower + 1)..*upper {
-                    borders.insert((i, *y_1));
+                for n in (*lower + 1)..*upper {
+                    borders.insert((n, *y_1));
                 }
             }
         }
     }
     let mut max = 0;
+    let mut count = 0;
     for (i, (x_1, y_1)) in tiles.iter().enumerate() {
         for (x_2, y_2) in tiles.iter().skip(i + 1) {
-            // +1 to include both tiles
+            println!("{}", count);
+            count += 1;
+            // Check opposite 2 corners are within the borders
+            if !(within_borders(x_1, y_2, &borders) && within_borders(x_2, y_1, &borders)) {
+                continue;
+            }
+            // +1 to include both tiles in area
             let dx = x_1.abs_diff(*x_2) + 1;
             let dy = y_1.abs_diff(*y_2) + 1;
             let area = dy * dx;
@@ -39,6 +51,33 @@ fn get_largest_rectangle_area(tiles: Vec<(u64, u64)>) -> u64 {
         }
     }
     max
+}
+
+fn within_borders(x: &u64, y: &u64, borders: &HashSet<(u64, u64)>) -> bool {
+    if borders.contains(&(*x, *y)) {
+        return true;
+    }
+    let mut below = false;
+    let mut above = false;
+    let mut left = false;
+    let mut right = false;
+
+    for (x_b, y_b) in borders {
+        if *x_b == *x && *y_b < *y {
+            above = true;
+        } else if *x_b == *x && *y_b > *y {
+            below = true;
+        } else if *x_b < *x && *y_b == *y {
+            left = true;
+        } else if *x_b > *x && *y_b == *y {
+            right = true;
+        }
+
+        if (above && below) || (right && left) {
+            return true;
+        }
+    }
+    false
 }
 
 fn read_input() -> Vec<(u64, u64)> {
